8d923d18656f3e73a3297c3853d29ebf6547ec95
Yaroslav Stasiuk <yastasyuk@gmail.com>
2019-10-27 01:54:09 +0300

    Initial Commit

---
+++ WhatTheFoxSay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatTheFoxSay
+//
+//  Created by Stasiuk Yaroslav on 27.10.2019.
+//  Copyright © 2019 Stasiuk Yaroslav. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
